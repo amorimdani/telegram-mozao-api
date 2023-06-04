614a563519f60e84f0aa8241545298e065a63ba6
Dani <contareserva230@gmail.com>
2023-06-03 23:40:32 -0300

    feat: voice

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
+const token = process.env.TELEGRAM_TOKEN;
 app.get('/telegram', async (req, res) => {
-  const token = process.env.TELEGRAM_TOKEN;
   const text = req.query.text;
   const chatId = req.query.chat_id;
 
@@ -33,6 +33,38 @@ app.get('/telegram', async (req, res) => {
     return res.status(500).send({ status: 'deu erro meu filho' });
   }
 });
+
+app.get('/audio', async (req, res) => {
+  const chatId = req.query.chat_id;
+  const audio = req.query.audio;
+
+  let data = JSON.stringify({
+    "voice": audio || "https://www.myinstants.com/media/sounds/asmei.mp3",
+    "duration": null,
+    "disable_notification": false,
+    "reply_to_message_id": null,
+    "chat_id": chatId
+  });
+
+  let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: `https://api.telegram.org/${token}/sendVoice`,
+    headers: {
+      'accept': 'application/json',
+      'content-type': 'application/json'
+    },
+    data: data
+  };
+
+  try {
+    const { data } = await axios.request(config);
+    console.log(JSON.stringify(data));
+    return res.send(data);
+  } catch (error) {
+    return res.status(500).send({ status: 'deu erro meu filho' });
+  }
+})
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('api rodando gatona');
